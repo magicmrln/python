b11ef27c6b3128b84b8988d480cef01ef8bfaef6
magicmrln <101424787+magicmrln@users.noreply.github.com>
2023-11-21 22:46:20 +0100

    caesar
    caesar chiffre en- and decoder

---
+++ caesar.py
@@ -0,0 +1,24 @@
+def caesar(str, offset):
+    strOut = ""
+    for i in range(len(str)):
+        strOut += caesarChar(str[i], offset)
+    return strOut
+
+def caesarChar(char, offset):
+    base = 65 # 65(gross) oder 97(klein) ; base = start in der ascii tabelle
+    if char.islower():
+        base = 97
+    index = ord(char)
+    if index == 32: # leertasten check
+        return " "
+    newIndex = (index - base + offset) % 26 + base
+    char = chr(newIndex)
+    return char
+
+print("---------- caesar cipher ----------")
+inStr = input("string: ")
+inOffset = int(input("offset: "))
+print("")
+out = caesar(inStr, inOffset)
+print(out)
+print("")
